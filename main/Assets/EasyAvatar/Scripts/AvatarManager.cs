e4320fe27ad17992c43e807cf7a63960d6368ea2
Levi <34293774+levyadams@users.noreply.github.com>
2018-04-10 13:33:57 -0400

    Update AvatarManager.cs

--- main/Assets/EasyAvatar/Scripts/AvatarManager.cs
+++ main/Assets/EasyAvatar/Scripts/AvatarManager.cs
@@ -42,10 +42,10 @@ public class AvatarManager : MonoBehaviour
         //Get the MR from the object to be attached
         SkinnedMeshRenderer newMeshRenderer = ObjectToAdd.GetComponentInChildren<SkinnedMeshRenderer>();
 
-        //if either of them are null, send a warning to the user and return.
+        //if either of them are null, send a warning to the user and return. This could be more robust and tell the user which, I suppose.
         if (newMeshRenderer == null || avatarMeshRenderer == null)
         {
-            Debug.LogWarning("No Skinned Mesh Renderer attached to the game object! Make sure to export both objects with THE SAME rig attached to BOTH.");
+            Debug.LogWarning("No Skinned Mesh Renderer attached to one of the game objects! Make sure to export both objects with THE SAME rig attached to BOTH.");
             return;
         }
 
